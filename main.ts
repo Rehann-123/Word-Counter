a5893c7089c68d71ee83a8e8c8cb8787b958b97e
Rehan <nawaz.abbasi.pk@gmail.com>
2024-05-28 18:58:19 +0500

    Done

---
+++ main.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+let isrunning: boolean = true;
+
+while (isrunning) {
+  let loop = await inquirer.prompt({
+    name: "While",
+    type: "list",
+    message: "What would you like to do",
+    choices: ["Enter sentence", "Exit"],
+  });
+  if (loop.While === "Enter sentence") {
+    let user_input: { Sentence: string } = await inquirer.prompt({
+      name: "Sentence",
+      type: "input",
+      message: "Type in the sentence you wish to have the word count for",
+    });
+    let wordcount = user_input.Sentence.trim().split(" ");
+
+    console.log(`Total words in  your sentence are ${wordcount.length}`);
+  }
+  if (loop.While === "Exit") {
+    isrunning = false;
+  }
+}
